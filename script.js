78c866c21590ab8a2407426bcf24f8c0b14c4557
Saroj Kumar <53892172+sarojvrc@users.noreply.github.com>
2021-09-19 19:51:52 +0530

    updated

--- script.js
+++ script.js
@@ -17,15 +17,36 @@ async function getWeatherByLocation(city) {
 }
 
 function addWeatherToPage(data) {
+    const weathertype =  data.weather[0].main;
     const temp = KtoC(data.main.temp);
+    const feelslike = KtoC(data.main.feels_like);
+    const windspeed = data.wind.speed;
+    const humidity = data.main.humidity;
 
     const weather = document.createElement("div");
     weather.classList.add("weather");
 
     weather.innerHTML = `
         <h2><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /> ${temp}°C <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /></h2>
-        <small>${data.weather[0].main}</small>
+        
+        <small>${weathertype}</small> <br>
+        <small>Feels Like ${feelslike}°C</small> <br>
+        <small>Wind Speed ${windspeed} km/h</small> <br>
+        <small>Humidity ${humidity}%</small> <br>
+
     `;
+
+    if(weathertype === "Clouds"){
+        document.body.style.background = "linear-gradient(279deg, #86a0cc, #bfc0c0)";
+    } else if(weathertype === "Mist"){
+        document.body.style.background = "linear-gradient(279deg, #6696e6, #8a8e8e)"
+    } else if(weathertype === "Clear"){
+        document.body.style.background = "linear-gradient(279deg, #d6dde2, #8a8e8e)"
+    } else if(weathertype === "Rain"){
+        document.body.style.background = "linear-gradient(279deg, #ef8787, #758ebd)"
+    } else{
+        document.body.style.background = "linear-gradient(279deg, #bfbaba, #c9ccd2)"
+    }
 
     main.innerHTML = "";
 
